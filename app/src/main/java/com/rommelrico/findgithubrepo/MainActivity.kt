c4449cd38db8faae94dda75bb5941548b1e4254a
Rommel Rico <me@rommelrico.com>
2018-09-16 07:24:20 -0700

    Getting access to the button and setting an onclicklistener

--- app/src/main/java/com/rommelrico/findgithubrepo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/findgithubrepo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.rommelrico.findgithubrepo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.searchButton)
+        button.setOnClickListener {
+            
+        }
     }
 }
