abf3c69434b1a62994b00237179ff737568660e0
Rommel Rico <me@rommelrico.com>
2018-09-16 07:28:38 -0700

    Getting access to the search EditText field

--- app/src/main/java/com/rommelrico/findgithubrepo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/findgithubrepo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+
         val button = findViewById<Button>(R.id.searchButton)
         button.setOnClickListener {
             val intent = Intent(this, SearchResultActivity::class.java)
-            intent.putExtra("searchTerm", "Hello")
+            intent.putExtra("searchTerm", searchEditText.text.toString())
             startActivity(intent)
         }
     }
